3f62483c7eb68ab47e380258cb86df203394391a
Leo Feng <thcheetah777@gmail.com>
2023-11-07 19:28:45 +0200

    🎲 some stuff

--- enemy.gd
+++ enemy.gd
@@ -5,35 +5,39 @@ enum STATE { PATROL, CHASE }
 
 @export var patrol_speed = 50
 @export var chase_speed = 100
+@export var patrol_path: Curve2D
 
 @onready var sprite := $AnimatedSprite2D
 @onready var raycast := $PlayerRaycast
 @onready var nav_agent := $NavigationAgent2D
+@onready var forget_timer = $ForgetTimer
 
 var state = STATE.PATROL
-var old_x: float
 
 func _physics_process(delta: float) -> void:
-	match state:
-		STATE.PATROL: patrol_state(delta)
-		STATE.CHASE: chase_state(delta)
-
-func patrol_state(delta: float):
-	sprite.flip_h = position.x < old_x
-	old_x = position.x
-
 	raycast.look_at(Globals.player.position)
 
+	match state:
+		STATE.PATROL: patrol_state(delta)
+		STATE.CHASE: chase_state()
+
+func patrol_state(delta: float):
 	if raycast.get_collider() is Player:
 		state = STATE.CHASE
 
-func chase_state(delta: float):
+func chase_state():
 	var direction = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = direction * chase_speed
+	sprite.flip_h = direction.x < 0
+
+	if not raycast.get_collider() is Player and forget_timer.is_stopped():
+		forget_timer.start()
+
 	move_and_slide()
 
-func create_path() -> void:
+func _on_navigation_timer_timeout() -> void:
 	nav_agent.target_position = Globals.player.global_position
 
-func _on_navigation_timer_timeout() -> void:
-	create_path()
+func _on_forget_timer_timeout() -> void:
+	if not raycast.get_collider() is Player:
+		state = STATE.PATROL
